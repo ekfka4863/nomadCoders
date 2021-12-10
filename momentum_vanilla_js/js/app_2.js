da2af67f46e7f79f3697153f7f7228bc697fd2d8
ekfka4863 <70094833+ekfka4863@users.noreply.github.com>
2021-12-10 20:29:56 +0900

    [Add]New file: add new file about ReactJs

--- momentum_vanilla_js/js/app_2.js
+++ momentum_vanilla_js/js/app_2.js
@@ -8,6 +8,7 @@ const timeQuoteWeatherSection = document.querySelector("#timeQuoteWeatherSection
 const loginForm = document.querySelector("#loginForm");
 const loginInput = document.querySelector("#userName");
 const greeting = document.querySelector("#greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
@@ -34,37 +35,55 @@ function paintGreetings(username) {
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
+loginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
 
+  console.log(e);      // 객체 
+  console.log(loginInput.value);   // ekfka4863
+
+  const loginInputValue = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, loginInputValue);
+
+  const localStorageUserName = localStorage.getItem(USERNAME_KEY);
+
+  if (localStorageUserName !== null) {
+    nameSection.style.display = "none";
+
+  } else {
+
+  }
+})
 
 
 // event =================================================
 // 여기부터
 // window.addEventListener("load", function(e) {
 //   if (usernameThatTheUserWrote === null) {
-
+//     loginForm.classList.add(HIDDEN_CLASSNAME);
+//     loginForm.addEventListener("submit", onLoginSubmit);
 //   } else {
-
+//     paintGreetings(savedUsername); 
 //   }
 // });
 
-loginForm.addEventListener("submit", function(e) {
-  e.preventDefault();
-  const usernameThatTheUserWrote = loginInput.value; 
-  console.log(usernameThatTheUserWrote);   // ok
-  localStorage.setItem(USERNAME_KEY, usernameThatTheUserWrote);
-  console.log(localStorage.getItem(USERNAME_KEY));  // ok
+// loginForm.addEventListener("submit", function(e) {
+//   e.preventDefault();
+//   const usernameThatTheUserWrote = loginInput.value; 
+//   console.log(usernameThatTheUserWrote);   // ok
+//   localStorage.setItem(USERNAME_KEY, usernameThatTheUserWrote);
+//   console.log(localStorage.getItem(USERNAME_KEY));  // ok
 
-  if (usernameThatTheUserWrote === null) {
-    // 
-    // loginForm.addEventListener("submit", onLoginSubmit);
-  } else {
-    nameSection.classList.add(HIDDEN_CLASSNAME);
-    todoListSection.classList.remove(HIDDEN_CLASSNAME);
-    timeQuoteWeatherSection.classList.remove(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hello ${username}!`;
-  }
+//   if (usernameThatTheUserWrote === null) {
+//     // 
+//     // loginForm.addEventListener("submit", onLoginSubmit);
+//   } else {
+//     nameSection.classList.add(HIDDEN_CLASSNAME);
+//     todoListSection.classList.remove(HIDDEN_CLASSNAME);
+//     timeQuoteWeatherSection.classList.remove(HIDDEN_CLASSNAME);
+//     greeting.innerText = `Hello ${username}!`;
+//   }
 
-});
+// });
 
 
 
