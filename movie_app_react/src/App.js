52e2fd317bb449900af6265e3ff427a1d1a76ecf
ekfka4863 <70094833+ekfka4863@users.noreply.github.com>
2021-12-17 18:33:38 +0900

    [Docs/Feat]: fix TIL.md & sth about react-router-dom

--- movie_app_react/src/App.js
+++ movie_app_react/src/App.js
@@ -1,41 +1,23 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year");
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);   // API가 로딩 됐으면 loading을 false로 바꿔준다! 
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  console.log(movies);
-
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} alt="" />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/abot-us">
+          <h1>Hello</h1>
+        </Route>
+        {/* <Route path="/movie/:chillitomato"> */}
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
