801a8155532c4de5c7bb8baee00c800e52f8e0cd
ekfka4863 <70094833+ekfka4863@users.noreply.github.com>
2021-12-24 02:14:39 +0900

    [Feat]fetch API: make coinlist automatically using api

--- master_course_react_crypto_tracker/src/routes/Coins.tsx
+++ master_course_react_crypto_tracker/src/routes/Coins.tsx
@@ -1,20 +1,105 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-const Container = styled.div``;
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const CoinList = styled.ul``;
 
-const Coin = styled.li``;
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 15px;
+
+  a {
+    transition: color .3s ease-in-out;
+    display: block;
+    padding: 10px;
+  }
+
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
 
 const Title = styled.h1`
+  font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
 
+// typescript로 api에 대한 interface 만들기 
+interface CoinInterface {
+  id: string,
+  name: string,
+  symbol: string,
+  rank: number,
+  is_new: boolean,
+  is_active: boolean,
+  type: string,
+}
+
+// test용 
+// const coins = [
+//   {
+//     id: "btc-bitcoin",
+//     name: "Bitcoin",
+//     symbol: "BTC",
+//     rank: 1,
+//     is_new: false,
+//     is_active: true,
+//     type: "coin",
+//     },
+//     {
+//     id: "eth-ethereum",
+//     name: "Ethereum",
+//     symbol: "ETH",
+//     rank: 2,
+//     is_new: false,
+//     is_active: true,
+//     type: "coin",
+//     },
+//     {
+//     id: "hex-hex",
+//     name: "HEX",
+//     symbol: "HEX",
+//     rank: 3,
+//     is_new: false,
+//     is_active: true,
+//     type: "token",
+//     },
+// ];
+// cf.  https://api.coinpaprika.com/v1/coins
 
 
 function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  useEffect(() => {
+    // 즉시실행함수
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      // console.log(json);              // 9000개가 넘음... 
+      setCoins(json.slice(0, 100));      // TIP! 100개만 가져오기! 
+    })();
+  }, []);
+
+  console.log(coins);
+
   return (
     <Container>
       <Header>
@@ -22,7 +107,12 @@ function Coins() {
       </Header>
       
       <CoinList>
-        <Coin></Coin>
+        {/* <Coin></Coin> */}
+        {coins.map((coin) => ( 
+          <Coin key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+          </Coin>
+        ))}
       </CoinList>
     </Container>
   );
